d3fbbc613fe29a671fc3d22a0e53884052eb7949
Tom Lutze <lutze.tom@gmail.com>
2021-09-11 23:28:49 -0700

    antd test with example

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div className="App">
+    <h1>antd version: {version}</h1>
+    <DatePicker />
+    <Button type="primary" style={{ marginLeft: 8 }}>
+      Primary Button
+    </Button>
+  </div>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
