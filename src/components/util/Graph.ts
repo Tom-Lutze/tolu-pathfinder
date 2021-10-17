a2585c1618defc98d0f9d6205d7ac820382473c4
Tom Lutze <lutze.tom@gmail.com>
2021-10-17 12:19:22 -0700

    Implement basic graph component

--- src/components/util/Graph.ts
+++ src/components/util/Graph.ts
@@ -1,3 +1,14 @@
-const Graph = () => {};
+import { useState } from 'react';
+
+const Graph = function ([getGraph, setGraph]: any) {
+  return {
+    addNode(node: any) {
+      setGraph([...getGraph, node]);
+    },
+    getNodes() {
+      return getGraph.map((ele: any) => ele.position);
+    },
+  };
+};
 
 export default Graph;
