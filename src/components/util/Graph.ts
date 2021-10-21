85845a3fec20dbd980f2d4d5547a8b61bbc3f122
Tom Lutze <lutze.tom@gmail.com>
2021-10-20 22:30:11 -0700

    Implement edges in adjacency list

--- src/components/util/Graph.ts
+++ src/components/util/Graph.ts
@@ -4,14 +4,28 @@ const Graph = function ([graph, setGraph]: any) {
   return {
     addNode(node: any) {
       nodeIndex++;
-      setGraph({
-        ...graph,
-        [nodeIndex]: node,
-        state: { ...(graph.state ?? {}), activeNode: nodeIndex },
-      });
+      // setGraph({
+      //   ...graph,
+      //   [nodeIndex]: node,
+      //   state: { ...(graph.state ?? {}), activeNode: nodeIndex },
+      // });
+      const newGraph = { ...graph };
+      if (!newGraph.state) newGraph.state = {};
+      if (newGraph.state.activeNode !== undefined) {
+        const prevNode = newGraph[newGraph.state.activeNode];
+        if (!prevNode.edges) prevNode.edges = new Set();
+        prevNode.edges.add(nodeIndex);
+        newGraph[newGraph.state.activeNode] = prevNode; //todo
+      }
+      newGraph[nodeIndex] = node;
+      newGraph.state.activeNode = nodeIndex;
+      setGraph(newGraph);
     },
     getNodesIdx() {
       return Object.keys(graph).filter((key) => key !== 'state');
+    },
+    getNode(idx: string) {
+      return graph[idx];
     },
     getGraph() {
       return graph;
