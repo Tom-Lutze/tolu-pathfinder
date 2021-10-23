c87b9c422135139cb98ee96de91e55affaa02f4b
Tom Lutze <lutze.tom@gmail.com>
2021-10-23 11:34:36 -0700

    Add pathfinder component

---
+++ src/components/util/Pathfinder.ts
@@ -0,0 +1,11 @@
+import { GraphInterface } from '../interfaces/interfaces';
+
+export const Pathfinder = function (graph: GraphInterface) {
+  return {
+    bfs() {
+      const path: any[] = [];
+      //TODO implement bfs
+      return path;
+    },
+  };
+};
