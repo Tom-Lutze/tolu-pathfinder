ab1b4180d8a626f980d2ea056ec0feec7a1b5543
Tom Lutze <lutze.tom@gmail.com>
2021-10-24 12:51:43 -0700

    Implement bfs algorithm

--- src/components/util/Pathfinder.ts
+++ src/components/util/Pathfinder.ts
@@ -1,10 +1,35 @@
+import { LatLng } from 'leaflet';
 import { GraphInterface } from '../interfaces/interfaces';
 
 export const Pathfinder = function (graph: GraphInterface) {
   return {
+    checkRequirements() {
+      return (
+        graph.state.startNode &&
+        graph.state.endNode &&
+        Object.keys(graph.nodes).length > 1
+      );
+    },
     bfs() {
-      const path: any[] = [];
-      //TODO implement bfs
+      if (!this.checkRequirements()) return false;
+      const path: LatLng[] = [];
+      const startNodeIdx = graph.state.startNode;
+      const endNodeIdx = graph.state.endNode;
+      const queue = [startNodeIdx];
+      while (queue.length) {
+        const current = queue.shift();
+        if (!current) {
+          continue;
+        }
+        if (current == endNodeIdx) {
+          // found path
+          return;
+        }
+        const currentEdges = graph.nodes[current].edges;
+        if (currentEdges) {
+          queue.concat(Array.from(currentEdges));
+        }
+      }
       return path;
     },
   };
