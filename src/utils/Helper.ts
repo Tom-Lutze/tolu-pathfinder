17158ac6429092e8cc57a9a23bddc596865ca4e6
Tom Lutze <lutze.tom@gmail.com>
2021-11-28 20:28:14 -0800

    Add helper functions to generate numbers in range

--- src/utils/Helper.ts
+++ src/utils/Helper.ts
@@ -1,3 +1,18 @@
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function getRandomIntExcept(min: number, max: number, except: number) {
+  if (except > max) {
+    throw new Error('The excepted number is bigger than the maximum');
+  }
+  const number = getRandomInt(min, max - 1);
+  if (number < except) return number;
+  else return number + 1;
+}
