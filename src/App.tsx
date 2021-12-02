689d02d9fdb42cf1e2b1638eca04a51fe77a1396
Tom Lutze <lutze.tom@gmail.com>
2021-12-01 21:48:19 -0800

    Adjust app layout and make sidebar collapsible

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,9 @@ const App = () => {
     <Layout>
       <Header className="header">
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
       </Header>
       <Layout>
-        <Sider width={200} className="site-layout-background">
+        <Sider width={200} className="site-layout-background" collapsible>
           <Menu
             mode="inline"
             defaultSelectedKeys={['1']}
@@ -53,16 +48,10 @@ const App = () => {
             </SubMenu>
           </Menu>
         </Sider>
-        <Layout style={{ padding: '0 24px 24px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
+        <Layout>
           <Content
             className="site-layout-background"
             style={{
-              padding: 24,
               margin: 0,
               minHeight: 280,
             }}
