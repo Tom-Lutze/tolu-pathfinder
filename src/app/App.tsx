90b043336eae8f15b859b39ba9110ce53ad0eb95
Tom Lutze <lutze.tom@gmail.com>
2022-01-01 13:10:52 -0800

    Adjust sidebar layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,26 @@
 import { Layout } from 'antd';
 import 'antd/dist/antd.css';
-import React, { useContext } from 'react';
+import React from 'react';
 import MapComponent from '../app/map/MapComponent';
-import { MenuTypes } from '../interfaces';
 import './App.css';
 import AppMenu from './ui/Menu';
-// import { storeContext } from '../utils/Store';
 
 const App = () => {
-  // const globalState = useContext(storeContext);
-  // const { appState } = globalState;
-
-  const { Header, Content, Sider } = Layout;
+  const { Content, Sider } = Layout;
   return (
     <Layout>
-      <Header className="header">
-        <div className="logo" />
-      </Header>
       <Layout>
-        <Sider width={200} className="site-layout-background" collapsible>
+        <Sider
+          style={{
+            overflow: 'auto',
+            height: '100vh',
+          }}
+          collapsible
+        >
+          <div className="logo" />
           <AppMenu />
         </Sider>
-        <Layout>
+        <Layout className="site-layout">
           <Content
             className="site-layout-background"
             style={{
@@ -29,10 +28,7 @@ const App = () => {
               minHeight: 280,
             }}
           >
-            <MapComponent
-            // graphType={appState.menu[MenuTypes.Graph]}
-            // algoType={appState.menu[MenuTypes.Algo]}
-            />
+            <MapComponent />
           </Content>
         </Layout>
       </Layout>
