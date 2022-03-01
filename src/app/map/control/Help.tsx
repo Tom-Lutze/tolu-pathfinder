732fa5730c2edd2bdb1c0f1c398ae43a370d5bcd
Tom Lutze <lutze.tom@gmail.com>
2022-02-28 20:51:57 -0800

    Integrate raw modal

--- src/app/map/control/Help.tsx
+++ src/app/map/control/Help.tsx
@@ -1,4 +1,5 @@
 import { QuestionCircleOutlined } from '@ant-design/icons';
+import { Button, Modal } from 'antd';
 import L from 'leaflet';
 import { useEffect, useRef, useState } from 'react';
 
@@ -6,22 +7,43 @@ import { useEffect, useRef, useState } from 'react';
  * Provides some guidelines how to use the app.
  */
 export const Help = () => {
-  const [showStatistics, setShowStatistics] = useState(false);
-  const controlLayerRef = useRef<any>();
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
+  const HelpModal = () => {
+    return (
+      <>
+        <Modal
+          title="Basic Modal"
+          visible={isModalVisible}
+          onCancel={() => setIsModalVisible(false)}
+          footer={null}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </>
+    );
+  };
+
+  const controlLayerRef = useRef<any>();
   // prevent forwarding of click events to the map component
   useEffect(() => {
     if (controlLayerRef) {
       L.DomEvent.disableClickPropagation(controlLayerRef.current);
       L.DomEvent.disableScrollPropagation(controlLayerRef.current);
     }
-  }, [showStatistics]);
+  });
 
   return (
     <div ref={controlLayerRef}>
       <div className="leaflet-bottom leaflet-left tolu-fade-comp">
         <div className="leaflet-control leaflet-bar">
-          <a className="tolu-help">
+          <HelpModal />
+          <a
+            className="tolu-help"
+            onClick={() => setIsModalVisible(!isModalVisible)}
+          >
             <QuestionCircleOutlined />
           </a>
         </div>
