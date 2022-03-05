929109bb2aa1825bd575444c4f8a634f17e970c1
Tom Lutze <lutze.tom@gmail.com>
2022-03-05 14:51:42 -0800

    Integrate new info and instructions

--- src/app/map/control/Help.tsx
+++ src/app/map/control/Help.tsx
@@ -1,9 +1,12 @@
 import { QuestionCircleOutlined } from '@ant-design/icons';
-import { Modal } from 'antd';
+import { Divider, Modal } from 'antd';
 import L from 'leaflet';
 import { useEffect, useRef, useState } from 'react';
 import { appStrings } from '../../constants/Strings';
 import parse from 'html-react-parser';
+import helpConnectImage from '../../../assets/images/help-connect.png';
+import helpStartEndImage from '../../../assets/images/help-start-end.png';
+import helpPathResultImage from '../../../assets/images/help-path-result.png';
 
 /**
  * Provides some guidelines how to use the app.
@@ -18,9 +21,30 @@ export const Help = () => {
           title={appStrings.helpModalTitle}
           visible={isModalVisible}
           onCancel={() => setIsModalVisible(false)}
-          footer={null}
+          cancelText="OK"
+          className="tolu-help-modal"
         >
-          {parse(appStrings.helpModalText)}
+          <p>{parse(appStrings.helpModalTextSec1)}</p>
+          <Divider orientation="left">
+            {appStrings.helpModalDividerTitle}
+          </Divider>
+          <p>{parse(appStrings.helpModalTextSec2)}</p>
+          <img
+            src={helpConnectImage}
+            alt={appStrings.helpModalConnectImgAlt}
+          ></img>
+          <p>{parse(appStrings.helpModalTextSec3)}</p>
+          <img
+            src={helpStartEndImage}
+            alt={appStrings.helpModalStartEndImgAlt}
+          ></img>
+          <p>{parse(appStrings.helpModalTextSec4)}</p>
+          <img
+            src={helpPathResultImage}
+            alt={appStrings.helpModalPathResultImgAlt}
+          ></img>
+          <p>{parse(appStrings.helpModalTextSec5)}</p>
+          <p>{parse(appStrings.helpModalTextSec6)}</p>
         </Modal>
       </>
     );
